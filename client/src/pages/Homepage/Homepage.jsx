f47c241b5f4fb189b2acccc47075c119e9f09fe5
Alenia24 <alenia.allenn@gmail.com>
2026-01-17 22:01:37 -0500

    Create and import navbarr component

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../Homepage/Homepage.css"
+import "../../components/NavBarr/NavBarr.jsx"
 
 function Homepage() {
   return (
     <>
       <div className="announcementBar">New Arrivals Just Dropped || <span><strong>Shop Now!!</strong></span></div>
-      <div>Homepage</div>
+      <NavBarr/>
     </>
   );
 }
